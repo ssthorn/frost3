61fa23abead096f70721360035c3c408c4b232ef
Sam <sam.stearn@gmail.com>
2023-09-06 18:29:24 -0700

    initial, header, footer

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Nav from '../components/Nav.jsx'
+import Footer from '../components/Footer.jsx'
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
+        <Nav />
+        <Footer />
         <NextScript />
       </body>
     </Html>

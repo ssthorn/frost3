5992c59f79ba23b1a5b7c69f43a752648b8133be
Sam <sam.stearn@gmail.com>
2023-09-19 11:50:18 -0700

    i

--- components/Community.jsx
+++ components/Community.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Community = () => {
   return (
     <div id='community'>
-        <div className='w-full text-center'>
-        <h1 className="px-2 mb-2 mt-0 text-6xl font-extrabold leading-tight">Our Legal Community</h1>
-
-        </div>
-
+      <div className='w-full text-center'>
+        <h1 className='px-2 mb-2 mt-0 text-6xl font-extrabold leading-tight'>
+          Our Legal Community
+        </h1>
+        <p className='lg:text-2xl text-xl text-left p-4'>
+          We are aggressive in legal matters, but we show a different side when
+          it comes to our firm&apos;s culture and our commitment to legal education.
+          We care deeply about providing on-the-job mentorship and creating
+          opportunities for gifted aspiring lawyers from underrepresented
+          backgrounds. Frost LLP is proud to support community initiatives that
+          are true to our vision of the legal profession&apos;s future. Frost LLP
+          founding partner Christopher Frost serves as an adjunct professor at
+          his alma mater, the Caruso School of Law (CSOL) at Pepperdine
+          University. Inspired by the impact of the Suit Stipend Project, which
+          provides financial support for first-generation law students needing
+          to purchase suits for their first professional interviews, Frost LLP
+          is endowing the Frost LLP and Frost Hassid Advisors Dress for Success
+          Suit Stipend Program.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Community
+export default Community;
